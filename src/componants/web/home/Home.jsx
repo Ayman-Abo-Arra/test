3239eef9c69b0c3a51a353fed5cfac4f394d9098
Ayman-Abo-Arra <Ayman.Abo.Arra10@gmail.com>
2023-12-23 12:42:30 +0200

    Full Project 1

--- src/componants/web/home/Home.jsx
+++ src/componants/web/home/Home.jsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <>
 <div id="Home" className="header vh-100 "> 
+<img src="../images/header1.jpg" alt="Online Shop" className=' position-absolute w-100 h-100 '/>
+
   <div className="container d-flex align-items-center h-100 font1 ">
-    <div className="header-contant text-white w-100  d-flex flex-column  align-items-end">
+    <div className="header-contant text-white w-100 position-relative  d-flex flex-column  align-items-end">
       <h1> Online Shop</h1>
       
       <h2> Clothes,Mobile,Electronics &amp;Beauty  </h2>
@@ -14,10 +16,6 @@ export default function Home() {
     </div>
   </div>
 </div>
-
-
-
-
     <Categories />
     </>
   )
